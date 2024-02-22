02885b534c25714fb78529e53ca7775798a37efc
seeyou7 <5764@holbertonstudents.com>
2024-02-22 16:32:35 +0100

    fix the next page after logout to login

--- LogScanner/accounts/urls.py
+++ LogScanner/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import register
+from . import views
+
 
 urlpatterns = [
     path('register/', register, name='register'),  #  registration
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),  # Built-in login view
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),  # Built-in logout view
+    
 ]
