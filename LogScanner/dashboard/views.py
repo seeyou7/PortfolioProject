8ccd4d4175844700b9ec6eb3caad50950a1774d1
seeyou7 <5764@holbertonstudents.com>
2024-02-25 14:32:02 +0100

    def the dashboard button logic

--- LogScanner/dashboard/views.py
+++ LogScanner/dashboard/views.py
@@ -5,3 +5,17 @@ from django.contrib.auth.decorators import login_required
 @login_required 
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html', {})
+
+def app_logs_view(request):
+
+    return render(request, 'dashboard/app_logs.html', {'is_analysis_page': True})
+
+#  Authentication Logs
+def auth_logs_view(request):
+
+    return render(request, 'dashboard/auth_logs.html', {'is_analysis_page': True})
+
+#  Network Logs
+def network_logs_view(request):
+
+    return render(request, 'dashboard/network_logs.html', {'is_analysis_page': True})
